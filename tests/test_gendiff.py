33b69b1c3195d8b8eaee7f1658f9aa1701f4f381
Yulia <jilly-2005@yandex.ru>
2024-03-16 14:33:44 +0300

    linter mistakes fixed in test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,5 @@
 from gendiff.scripts.gendiff import generate_diff
 
-
 file_stylish = open('./tests/fixtures/expected_stylish.txt', 'r')
 result_stylish = file_stylish.read()
 file_plain = open('./tests/fixtures/expected_plain.txt', 'r')
@@ -14,48 +13,88 @@ result_nested_plain = file_nested_plain.read()
 file_nested_json = open('./tests/fixtures/expected_nested_json.txt', 'r')
 result_nested_json = file_nested_json.read()
 
+
 def test_flat_files_stylish():
-    assert result_stylish == generate_diff('./tests/fixtures/test_before.json',
-                                   './tests/fixtures/test_after.json')
-    assert result_stylish == generate_diff('./tests/fixtures/test_before.yml',
-                                   './tests/fixtures/test_after.yml')
+    assert result_stylish == \
+           generate_diff('./tests/fixtures/'
+                         'test_before.json',
+                         './tests/fixtures/'
+                         'test_after.json')
+    assert result_stylish == \
+           generate_diff('./tests/fixtures/'
+                         'test_before.yml',
+                         './tests/fixtures/'
+                         'test_after.yml')
 
 
 def test_flat_files_plain():
-    assert result_plain == generate_diff('./tests/fixtures/test_before.json',
-                                   './tests/fixtures/test_after.json',"plain")
-    assert result_plain == generate_diff('./tests/fixtures/test_before.yml',
-                                   './tests/fixtures/test_after.yml',"plain")
+    assert result_plain == \
+           generate_diff('./tests/'
+                         'fixtures/test_before.json',
+                         './tests/'
+                         'fixtures/test_after.json',
+                         "plain")
+    assert result_plain == \
+           generate_diff('./tests/'
+                         'fixtures/test_before.yml',
+                         './tests/'
+                         'fixtures/test_after.yml',
+                         "plain")
 
 
 def test_nested_files_stylish():
-    assert result_nested_stylish == generate_diff('./tests/fixtures/test_before_nested.json',
-                                          './tests/fixtures/test_after_nested.json',
-                                          "stylish")
-    assert result_nested_stylish == generate_diff('./tests/fixtures/test_before_nested.yaml',
-                                          './tests/fixtures/test_after_nested.yml',
-                                          "stylish")
+    assert result_nested_stylish == \
+           generate_diff('./tests/'
+                         'fixtures/test_before_nested.json',
+                         './tests/'
+                         'fixtures/test_after_nested.json',
+                         "stylish")
+    assert result_nested_stylish == \
+           generate_diff('./tests/'
+                         'fixtures/test_before_nested.yaml',
+                         './tests/'
+                         'fixtures/test_after_nested.yml',
+                         "stylish")
 
 
 def test_nested_files_plain():
-    assert result_nested_plain == generate_diff('./tests/fixtures/test_before_nested.json',
-                                                './tests/fixtures/test_after_nested.json',
-                                                "plain")
-    assert result_nested_plain == generate_diff('./tests/fixtures/test_before_nested.yaml',
-                                                './tests/fixtures/test_after_nested.yml',
-                                                "plain")
+    assert result_nested_plain == \
+           generate_diff('./tests/'
+                         'fixtures/test_before_nested.json',
+                         './tests/'
+                         'fixtures/test_after_nested.json',
+                         "plain")
+    assert result_nested_plain == \
+           generate_diff('./tests/'
+                         'fixtures/test_before_nested.yaml',
+                         './tests/'
+                         'fixtures/test_after_nested.yml',
+                         "plain")
+
 
 def test_flat_files_json():
-    assert result_json == generate_diff('./tests/fixtures/test_before.json',
-                                   './tests/fixtures/test_after.json',"json")
-    assert result_json == generate_diff('./tests/fixtures/test_before.yml',
-                                   './tests/fixtures/test_after.yml',"json")
+    assert result_json == \
+           generate_diff('./tests/fixtures/'
+                         'test_before.json',
+                         './tests/fixtures/'
+                         'test_after.json', "json")
+    assert result_json == \
+           generate_diff('./tests/fixtures/'
+                         'test_before.yml',
+                         './tests/fixtures/'
+                         'test_after.yml',
+                         "json")
 
 
 def test_nested_files_json():
-    assert result_nested_json == generate_diff('./tests/fixtures/test_before_nested.json',
-                                          './tests/fixtures/test_after_nested.json',
-                                          "json")
-    assert result_nested_json == generate_diff('./tests/fixtures/test_before_nested.yaml',
-                                          './tests/fixtures/test_after_nested.yml',
-                                          "json")
+    assert result_nested_json == \
+           generate_diff('./tests/fixtures/'
+                         'test_before_nested.json',
+                         './tests/fixtures/'
+                         'test_after_nested.json',
+                         "json")
+    assert result_nested_json == \
+           generate_diff('./tests/fixtures/'
+                         'test_before_nested.yaml',
+                         './tests/fixtures/'
+                         'test_after_nested.yml', "json")
